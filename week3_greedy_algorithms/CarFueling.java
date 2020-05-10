a90c93aa30ad66689ab651e70888c5d7c22fa6f1
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2020-05-11 00:25:16 +0530

    Greedy -algo- carfueling done

--- week3_greedy_algorithms/CarFueling.java
+++ week3_greedy_algorithms/CarFueling.java
@@ -2,8 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class CarFueling {
-    static int computeMinRefills(int dist, int tank, int[] stops) {
-        return -1;
+    static int computeMinRefills(int dist, int tank, int[] A) {
+    	int currFill =0, lastFill, fill=0;
+    	if (dist> A[A.length-2])
+    		A[A.length-1] = dist;
+    		
+//    	for (int i: A)
+//    	{
+//    		System.out.println(i);
+//    	}
+    		while (A[currFill] < dist)
+    	{
+    		lastFill = currFill;
+    	//	System.out.println("lastfill = "+ lastFill + "currfill = "+ currFill);
+    		//System.out.println((A[currFill+1]- A[lastFill]) + "<= tank");
+    		while (A[currFill] < dist && A[currFill+1]- A[lastFill] <= tank)
+    		{
+    			currFill++;
+    			//System.out.println(currFill);
+    		}
+    		
+    		//System.out.println("last currfill = "+currFill);
+    		
+    		if (currFill == lastFill )
+    			return -1;
+    		if (A[currFill] < dist)
+    			{fill++;
+    			//System.out.println("fill = "+fill);
+    			
+    			}
+    		
+    	}
+    			
+    	
+        return fill;
     }
 
     public static void main(String[] args) {
@@ -11,11 +43,13 @@ public class CarFueling {
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
         int n = scanner.nextInt();
-        int stops[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        int stops[] = new int[n+2];
+        stops[0]=0;
+        for (int i = 1; i < n+1; i++) {
             stops[i] = scanner.nextInt();
         }
 
         System.out.println(computeMinRefills(dist, tank, stops));
     }
 }
+
